2ed95374ead95d47c61bfcc27f9601bd6f223ad6
Nico Gufron <nicogufron@gmail.com>
2019-12-12 15:58:24 +0700

    Added javascript

--- js/kerja.js
+++ js/kerja.js
@@ -1,5 +1,5 @@
 var pass = document.getElementById("password");
-var button = document.getElementById("submit");
+var button = document.getElementById("klik");
 button.onclick = function(){
     if(pass.value == ""){
         alert('Type in your password');
