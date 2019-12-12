bbe2c560300f6c30edd6cedb1f29e42624631e40
Nico Gufron <nicogufron@gmail.com>
2019-12-12 16:10:27 +0700

    revision

--- js/kerja.js
+++ js/kerja.js
@@ -7,11 +7,18 @@ var rijndael = document.getElementById("rijndael");
 var caesar = document.getElementById("caesar");
 var playfair = document.getElementById("playfair");
 var vernam = document.getElementById("vernam");
-    button.onclick = function(){
+    enc.onclick = function(){
         if(pass.value == ""){
             alert('Type in your password');
         }else{
             rijndael.innerHTML = pass.value;
+        }
+    }
+    dec.onclick = function(){
+        if(pass.value == ""){
+            alert('Type in your password');
+        }else{
+            caesar.innerHTML = pass.value;
         }
     }
 }
